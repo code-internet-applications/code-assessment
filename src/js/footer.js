b12cea88f01160bdd263067d8c3006ec62b4d605
FHoogland <fhoogland@live.nl>
2024-03-20 11:56:04 +0100

    Question 6: Accordion for links

--- src/js/footer.js
+++ src/js/footer.js
@@ -45,3 +45,71 @@ class CollapsibleFooter extends Collapsible {
 if (!customElements.get('collapsible-footer')) {
   customElements.define('collapsible-footer', CollapsibleFooter);
 }
+
+let visibility1 = false
+
+clickable1 = document.getElementById('accordion-header')
+accordion1Content = document.getElementById('accordion-1-content')
+accordion1Content.style.display = "none";
+
+
+clickable1.addEventListener("click", () => {
+  visibility1 = !visibility1;
+  if (visibility1) {
+    accordion1Content.style.display = "block";
+  } else {
+    accordion1Content.style.display = "none";
+
+  }
+
+});
+
+let visibility2 = false
+
+clickable2 = document.getElementById('accordion-header-2')
+accordion2Content = document.getElementById('accordion-2-content')
+accordion2Content.style.display = "none";
+
+clickable2.addEventListener("click", () => {
+  visibility2 = !visibility2;
+  if (visibility2) {
+    accordion2Content.style.display = "block";
+  } else {
+    accordion2Content.style.display = "none";
+
+  }
+});
+
+
+let visibility3 = false
+
+clickable3 = document.getElementById('accordion-header-3')
+accordion3Content = document.getElementById('accordion-3-content')
+accordion3Content.style.display = "none";
+
+clickable3.addEventListener("click", () => {
+  visibility3 = !visibility3;
+  if (visibility3) {
+    accordion3Content.style.display = "block";
+  } else {
+    accordion3Content.style.display = "none";
+
+  }
+});
+
+
+let visibility4 = false
+
+clickable4 = document.getElementById('accordion-header-4')
+accordion4Content = document.getElementById('accordion-4-content')
+accordion4Content.style.display = "none";
+
+clickable4.addEventListener("click", () => {
+  visibility4 = !visibility4;
+  if (visibility4) {
+    accordion4Content.style.display = "block";
+  } else {
+    accordion4Content.style.display = "none";
+
+  }
+});
